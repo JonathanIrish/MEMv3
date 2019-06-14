c621cff362a6ea3d94eb4ec2409607c2463f6ce9
Sierra Barone <sierrambarone@gmail.com>
2019-06-14 10:38:26 -0500

    Cleaned up formatting

--- R/create_labels_txt.R
+++ R/create_labels_txt.R
@@ -1,43 +1,58 @@
-create.labels.txt <- function(MEM_vals_scale, display.thresh,heatmap_data){
-  posRownamesMatrix = matrix()
-  negRownamesMatrix = matrix()
-  for(i in 1:nrow(heatmap_data)){
+create.labels.txt <-
+    function(MEM_vals_scale,
+             display.thresh,
+             heatmap_data) {
+        posRownamesMatrix = matrix()
+        negRownamesMatrix = matrix()
+        for (i in 1:nrow(heatmap_data)) {
+            #Get markers above threshold
+            posMarkers = MEM_vals_scale[i, ][MEM_vals_scale[i, ] >= display.thresh]
+            negMarkers = MEM_vals_scale[i, ][MEM_vals_scale[i, ] < 0 &
+                                                 abs(MEM_vals_scale[i, ]) >= display.thresh]
+            #Order markers by abs(MEM)
+            posMarkersOrdered = posMarkers[order(posMarkers, decreasing = TRUE)]
+            negMarkersOrdered = negMarkers[order(abs(negMarkers), decreasing = TRUE)]
 
-    #     Get markers above threshold
-    posMarkers = MEM_vals_scale[i,][MEM_vals_scale[i,]>=display.thresh]
-    negMarkers = MEM_vals_scale[i,][MEM_vals_scale[i,]<0 & abs(MEM_vals_scale[i,])>=display.thresh]
+            #Get + or - sign for each corresponding marker
+            posVector = rep("+", length(posMarkers))
+            negVector = rep("-", length(negMarkers))
 
-    #     posMarkers = MEM_vals_scale[i,][1:3]
-    #     negMarkers = MEM_vals_scale[i,][1:3]
-    #Order markers by abs(MEM)
-    posMarkersOrdered = posMarkers[order(posMarkers,decreasing=TRUE)]
-    negMarkersOrdered = negMarkers[order(abs(negMarkers),decreasing=TRUE)]
+            if (length(posVector) == 0) {
+                posVector = rep("None", 1)
+            }
 
-    #Get + or - sign for each corresponding marker
-    posVector = rep("+",length(posMarkers))
-    negVector = rep("-",length(negMarkers))
+            if (length(negVector) == 0) {
+                negVector = rep("None", 1)
+            }
 
-    if (length(posVector)==0){
-        posVector = rep("None",1)}
+            #Put together character vectors for rownames
+            posSignedVec = as.matrix(paste(posVector, abs(round(
+                posMarkersOrdered, 2
+            )), sep = ""))
+            posMarkersRanked = as.matrix(names(posMarkersOrdered))
 
-    if (length(negVector)==0){
-        negVector = rep("None",1)}
+            posRownames = t(paste(posMarkersRanked, posSignedVec, sep = ""))
+            posRownamesMatrix[i] = apply(posRownames, 1, paste, collapse = " ")
 
-    #Put together character vectors for rownames
-    posSignedVec = as.matrix(paste(posVector,abs(round(posMarkersOrdered,2)),sep=""))
-    posMarkersRanked = as.matrix(names(posMarkersOrdered))
+            negSignedVec = as.matrix(paste(negVector, abs(round(
+                negMarkersOrdered, 2
+            )), sep = ""))
+            negMarkersRanked = as.matrix(names(negMarkersOrdered))
 
-    posRownames = t(paste(posMarkersRanked,posSignedVec,sep=""))
-    posRownamesMatrix[i] = apply(posRownames,1,paste,collapse=" ")
+            negRownames = t(paste(negMarkersRanked, negSignedVec, sep = ""))
+            negRownamesMatrix[i] = apply(negRownames, 1, paste, collapse = " ")
+        }
+        dot = rawToChar(as.raw(149))
 
-    negSignedVec = as.matrix(paste(negVector,abs(round(negMarkersOrdered,2)),sep=""))
-    negMarkersRanked = as.matrix(names(negMarkersOrdered))
-
-    negRownames = t(paste(negMarkersRanked,negSignedVec,sep=""))
-    negRownamesMatrix[i] = apply(negRownames,1,paste,collapse=" ")
-  }
-  dot = rawToChar(as.raw(149))
-
-  new_rownames = paste(row.names(MEM_vals_scale),":",'UP',posRownamesMatrix,paste(dot),'DN',negRownamesMatrix,sep=" ")
-  return(new_rownames)
-}
+        new_rownames = paste(
+            row.names(MEM_vals_scale),
+            ":",
+            'UP',
+            posRownamesMatrix,
+            paste(dot),
+            'DN',
+            negRownamesMatrix,
+            sep = " "
+        )
+        return(new_rownames)
+    }
--- R/get_files.R
+++ R/get_files.R
@@ -1,37 +1,38 @@
-# Determine extension type and read in multiple files
-
-get.files <- function(exp_data){
-
-    # Determine file extension
+get.files <- function(exp_data) {
+    # Determine extension type and read in multiple files
     ext = file_ext(exp_data[[1]])
 
     # For FCS files
-    if (ext == "fcs"){
-    fileList = list.files(pattern="*.fcs")
-    allFiles = lapply(fileList, read.FCS)
-    allMatrices = lapply(allFiles, exprs)
+    if (ext == "fcs") {
+        fileList = list.files(pattern = "*.fcs")
+        allFiles = lapply(fileList, read.FCS)
+        allMatrices = lapply(allFiles, exprs)
     }
 
     # For TXT files
-    if (ext == "txt"){
-    fileList = list.files(pattern="*.txt")
-    allMatrices = lapply(fileList, read.table, header=TRUE)
+    if (ext == "txt") {
+        fileList = list.files(pattern = "*.txt")
+        allMatrices = lapply(fileList, read.table, header = TRUE)
 
     }
     # For CSV Files
 
-    if(ext == "csv"){
-    fileList = list.files(pattern="*.csv")
-    allMatrices = lapply(fileList, read.table)
+    if (ext == "csv") {
+        fileList = list.files(pattern = "*.csv")
+        allMatrices = lapply(fileList, read.table)
     }
 
     # Return ordered list of files for user reference
-    dir.create("./output files/",showWarnings=FALSE)
-    write.table(exp_data,paste("./output files/",strftime(Sys.time(),"%Y-%m-%d_%H%M%S")," File order.txt"),col.names=FALSE)
+    dir.create("./output files/", showWarnings = FALSE)
+    write.table(exp_data,
+                paste(
+                    "./output files/",
+                    strftime(Sys.time(), "%Y-%m-%d_%H%M%S"),
+                    " File order.txt"
+                ),
+                col.names = FALSE)
 
     names(allMatrices) = c(1:length(allMatrices))
     return(allMatrices)
 
 }
-
-
